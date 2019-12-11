0beb40d6be5b08eff4957b7d545a34b1fc4766e8
Francisco Silva <ischawk@gmail.com>
2019-12-11 14:12:26 -0600

    Updating database scripts

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(id INTEGER AUTO_INCREMENT PRIMARY KEY,
+                     burger_name VARCHAR(100),
+                     devoured BOOLEAN);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name,devoured) VALUES("Krusty Burger con Queso",false);
+INSERT INTO burgers(burger_name,devoured) VALUES("Krusty Burger Doble con Queso",true);
+INSERT INTO burgers(burger_name,devoured) VALUES("Krusty Leche no lactea parcialmente gelatinada",false);
+COMMIT;
